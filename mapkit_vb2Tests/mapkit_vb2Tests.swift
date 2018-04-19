375f5e5e1683288694eb0e02db21e69476a3cf18
Johan van den Broek <Johan@atheoi.local>
2018-04-19 11:26:53 +0200

    Initial Commit

---
+++ mapkit_vb2Tests/mapkit_vb2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  mapkit_vb2Tests.swift
+//  mapkit_vb2Tests
+//
+//  Created by Johan van den Broek on 19/04/2018.
+//  Copyright © 2018 Johan van den Broek. All rights reserved.
+//
+
+import XCTest
+@testable import mapkit_vb2
+
+class mapkit_vb2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
